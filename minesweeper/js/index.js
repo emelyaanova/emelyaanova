7f691619d536d8b08fb03d2cfeeb383e78608ac5
emelyaanova <113417532+emelyaanova@users.noreply.github.com>
2023-01-08 13:47:57 +0700

    Add files via upload

---
+++ minesweeper/js/index.js
@@ -0,0 +1,84 @@
+import {aiMoveBtn, startBtn, GameView, clickToTail, canvas} from "./view.js"
+import {setMine} from "./game.js"
+import {start, stop, dimension, nMine, minedTrue, minedFalse, showTrue, showFalse, showFlag} from "./config.js"
+import {MinesweeperAI} from "./minesweeper.js"
+
+let state;
+let s1;
+var ai;
+startGame();
+
+startBtn.addEventListener('click', startGame);
+aiMoveBtn.addEventListener('click', aiMove);
+canvas.addEventListener('mouseup', moveView);
+
+function startGame()
+{
+	state = start;
+	s1 = setMine(dimension, nMine); 
+	ai = new MinesweeperAI(dimension);
+	new GameView(s1);
+}
+
+
+
+function moveView(event)
+{ 
+	if(state){
+		const [i, j] = clickToTail(event.offsetX, event.offsetY);
+		if(event.button === 0)
+		{ 
+			if(s1[i][j].mined !== minedTrue)
+			{ 
+				s1[i][j].show = showTrue;
+				ai.add_knowledge(i * dimension + j, s1[i][j].minesCount);
+			}
+			else 
+			{ 
+				state = stop;
+				for(let l = 0; l < dimension; ++l) 
+				for(let k = 0; k < dimension; ++k)
+				s1[l][k].show = showTrue;
+		}
+		}
+		else 
+		{ 
+			if(s1[i][j].show === showFalse)
+				s1[i][j].show = showFalse;
+			else if(s1[i][j].show === showFalse)
+			s1[i][j].show = showFlag;
+		} 
+		new GameView(s1)
+	}
+} 
+function aiMove()
+{
+	if(state)
+	{ 
+		let move = ai.make_safe_move(); 
+		if(move === undefined) 
+		{ 
+			move = ai.make_random_move(); 
+			if(move === undefined) 
+			{ }
+			} 
+			let i = Math.floor(move / dimension);
+			let j = move % dimension;
+			if(s1[i][j].mined !== minedTrue)
+			{
+				s1[i][j].show = showTrue;
+				ai.add_knowledge(move, s1[i][j].minesCount);
+			}
+			else
+				{ 
+				state = stop;
+				for(let l = 0; l < dimension; ++l)
+				for(let k = 0; k < dimension; ++k)
+				s1[l][k].show = showTrue;
+				}
+				ai.mines.forEach(m =>{let i = Math.floor(m / dimension); let j = m % dimension; s1[i][j].show = showFlag;}); 
+	new GameView(s1)
+}
+}
+
+
---
+++ minesweeper/js/test.js
@@ -0,0 +1,2 @@
+import (Sentence) from "./minesweeper.js"
+const s1 = new sentence 
